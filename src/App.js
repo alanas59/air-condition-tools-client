b61bb19fe8a4e4dbae46a619681888b6a68de4f0
al-anas <alanas7171@gmail.com>
2022-05-23 00:04:41 +0600

    banner component created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Pages/Shared/Header";
 import Footer from "./Pages/Shared/Footer";
+import Banner from "./Pages/Home/Banner";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Banner></Banner>
       <Footer></Footer>
     </div>
   );
