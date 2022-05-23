8e947630ed725ac25d3bbf311ad1914692d9bb75
al-anas <alanas7171@gmail.com>
2022-05-24 00:06:46 +0600

    firebase hooks and firebase installed and sign in with goolge implemented

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from "../../firebase.init";
+
 
 const Login = () => {
-    
+  const navigate = useNavigate();
+  const [signInWithGoogle, guser, gloading, gerror] = useSignInWithGoogle(auth);
+  
+  if(guser){
+    console.log(guser);
+    navigate('/');
+  }
   const handleLogin = (event) => {
     event.preventDefault();
     const email = event.target.email.value;
@@ -38,9 +47,16 @@ const Login = () => {
           </div>
           <input className="btn btn-info" type="submit" value="Login" />
         </form>
+        <p className="my-2 text-danger">{gerror?.message}</p>
         <p className="my-2">
           New to this website? <Link to="/register">Please register</Link>
         </p>
+        <div className="text-center mt-4">
+          <button 
+          className="btn btn-primary"
+          onClick={() => signInWithGoogle()}
+          >Google sign in</button>
+        </div>
       </div>
     </div>
   );
