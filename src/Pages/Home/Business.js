8fbb7a5e36c7e23a9207cfc567542e522c8bc05d
al-anas <alanas7171@gmail.com>
2022-05-23 22:35:03 +0600

    business component created

---
+++ src/Pages/Home/Business.js
@@ -0,0 +1,35 @@
+import React from "react";
+import CountUp from "react-countup";
+import { HiOutlineUserGroup, HiStar, HiTrendingUp } from "react-icons/hi";
+import "./Business.css";
+
+const Business = () => {
+  return (
+     <div className="container my-5">
+     <h2 className="text-center my-3">Business Summary</h2>
+      <div className="row justify-content-around">
+        {/* section-1 */}
+        <div className="col-lg-3 text-center border py-4 mt-2">
+          <p>
+            <HiOutlineUserGroup className="icon" />
+          </p>
+          <CountUp suffix="+ Customers" className="fs-5" end={100} />
+        </div>
+        {/* section-2*/}
+        <div className="col-lg-3 text-center border py-4 mt-2">
+          <p>
+            <HiStar className="icon" />
+          </p>
+          <CountUp className="fs-5" suffix="K+ Reviews" end={100} />
+        </div>
+        {/* section-2*/}
+        <div className="col-lg-3 text-center border py-4 mt-2">
+          <p><HiTrendingUp className="icon" /></p>
+          <CountUp className="fs-5" suffix="M+ Annual revenue" end={100} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Business;
