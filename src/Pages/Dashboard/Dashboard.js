482495b007bd0c6b54063115bbee1f01f98e122e
al-anas <alanas7171@gmail.com>
2022-05-24 22:45:47 +0600

    review component updated

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -4,23 +4,22 @@ import { Link, Outlet } from "react-router-dom";
 const Dashboard = () => {
   return (
     <div className="container my-5">
-      <div className="row">
+      <div className="row g-4">
         <div className="col-lg-4">
           <ul class="list-group">
             <li class="list-group-item">
-                <Link to="/dashboard/my-orders">My Orders</Link>
+              <Link to="/dashboard/my-orders">My Orders</Link>
             </li>
             <li class="list-group-item">
-                <Link to="/dashboard/add-review">Add a reivew</Link>
+              <Link to="/dashboard/add-review">Add a reivew</Link>
             </li>
             <li class="list-group-item">
-                <Link to="/dashboard/my-profile">My Profile</Link>
+              <Link to="/dashboard/my-profile">My Profile</Link>
             </li>
-         
           </ul>
         </div>
-        <div className="col-lg-8 border">
-          <h2 className="text-center">Your Dashboard</h2>
+        <div className="col-lg-8 ">
+          <h2 className="">Your Dashboard</h2>
           <Outlet />
         </div>
       </div>
--- src/Pages/Dashboard/Review.js
+++ src/Pages/Dashboard/Review.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
 
 const Review = () => {
-    return (
-        <div>
-           <h2>Review</h2> 
+  const handleReview = event =>{
+      event.preventDefault();
+      const rating = event.target.rating.value;
+      const description = event.target.description.value;
+      console.log(rating,description);
+  }
+  return (
+    <div className="col-lg-8 mt-2 border border-info p-4">
+      <h2>Review</h2>
+      <form onSubmit={handleReview}>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">
+            Please rate
+          </label>
+          <input
+            type="number"
+            name="rating"
+            min="1"
+            max="5"
+            class="form-control"
+            id="exampleFormControlInput1"
+           
+          />
         </div>
-    );
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">
+           Write Something
+          </label>
+          <textarea
+            class="form-control"
+            name="description"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+        <button className="btn btn-info">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Review;
