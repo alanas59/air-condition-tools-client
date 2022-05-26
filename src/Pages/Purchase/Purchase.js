87d733b36ae17e224c01034c08c3f827881a1688
al-anas <alanas7171@gmail.com>
2022-05-26 15:47:13 +0600

    purchase component updated

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,38 +1,68 @@
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 import auth from "../../firebase.init";
-
 
 const Purchase = () => {
   const { id } = useParams();
   const [product, setProduct] = useState({});
   const [user, loading] = useAuthState(auth);
-  const [minOrder,setMinOrder] = useState(0);
-  const [error,setError] = useState("");
+  const [minOrder, setMinOrder] = useState(0);
+  const [error, setError] = useState("");
   useEffect(() => {
     fetch(`http://localhost:5000/product/${id}`)
       .then((res) => res.json())
       .then((data) => {
-          setProduct(data);
-          setMinOrder(data.order);
-        });
+        setProduct(data);
+        setMinOrder(data.order);
+      });
   }, []);
-  const handleOrder = event =>{
-      console.log(product.order,product.quantity)
-     const newOrder = parseInt(event.target.value);
-
-     if(newOrder < product.order){
-         setError('Your order in less than minimum order');
+  const handleSubmit = event =>{
+     event.preventDefault();
+     const uname = user.displayName;
+     const email = user.email;
+     const quantity = event.target.quantity.value;
+     const address = event.target.address.value;
+     const phone = event.target.phone.value;
+     const price = product.price;
+     const productId = id;
+     const orderNow = {
+        uname,
+        email,
+        quantity,
+        address,
+        phone,
+        price,
+        productId
      }
-     else if(newOrder > product.quantity){
-         setError('Your order is greater than available product')
-     }
-     else{
-         setError("");
-     }
-     setMinOrder(newOrder);
+     fetch('http://localhost:5000/order',{
+       method:'POST',
+       headers:{
+         'content-type':'application/json'
+       },
+       body:JSON.stringify(orderNow)
+     })
+     .then(res => res.json())
+     .then(data => {
+       if(data.insertedId){
+         toast('Order successfully done.')
+       }
+     })
   }
+  const handleOrder = (event) => {
+    console.log(product.order, product.quantity);
+    const newOrder = parseInt(event.target.value);
+
+    if (newOrder < product.order) {
+      setError("Your order in less than minimum order");
+    } else if (newOrder > product.quantity) {
+      setError("Your order is greater than available product");
+    } else {
+      setError("");
+    }
+    setMinOrder(newOrder);
+  };
   return (
     <div className="container">
       <div className="row my-5">
@@ -50,80 +80,84 @@ const Purchase = () => {
         </div>
         <div className="col-lg-6 border p-4">
           {/* user */}
-         
-          <div className="border p-4">
-        <form>
-        <h2>Order Now</h2>
-          <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">
-             Your Name
-            </label>
-            <input
-              type="text"
-              class="form-control"
-              name="name"
-              disabled
-              value={user?.displayName}
-              id="exampleFormControlInput1"
-            />
-          </div>
-          <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">
-             Your Email
-            </label>
-            <input
-              type="email"
-              class="form-control"
-              name="email"
-              value={user?.email}
-              disabled
-              id="exampleFormControlInput1"
-            />
-          </div>
-          <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">
-             Your Address
-            </label>
-            <input
-              type="text"
-              class="form-control"
-              name="address"
-              id="exampleFormControlInput1"
-            />
-          </div>
-         
-          <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">
-            Your Phone
-            </label>
-            <input
-              type="text"
-              name="phone"
-              class="form-control"
-              id="exampleFormControlInput1"
-            />
-          </div>
-          <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">
-            Order quantity
-            </label>
-            <input
-              type="number"
-              name="quantity"
-              class="form-control"
-              value={minOrder}
-              onChange={handleOrder}
-              id="exampleFormControlInput1"
-            />
-          </div>
-          <p className="text-danger mt-2">{error}</p>
 
-          <button 
-          disabled={minOrder<product.order || minOrder>product.quantity} 
-          className="btn  btn-primary w-100 text-white" >Order Now</button>
-         
-        </form>
-      </div>
+          <div className="border p-4">
+            <form onSubmit={handleSubmit}>
+              <h2>Order Now</h2>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  name="name"
+                  disabled
+                  value={user?.displayName}
+                  id="exampleFormControlInput1"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">
+                  Your Email
+                </label>
+                <input
+                  type="email"
+                  class="form-control"
+                  name="email"
+                  value={user?.email}
+                  disabled
+                  id="exampleFormControlInput1"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">
+                  Your Address
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  name="address"
+                  id="exampleFormControlInput1"
+                />
+              </div>
+
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">
+                  Your Phone
+                </label>
+                <input
+                  type="text"
+                  name="phone"
+                  class="form-control"
+                  id="exampleFormControlInput1"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">
+                  Order quantity
+                </label>
+                <input
+                  type="number"
+                  name="quantity"
+                  class="form-control"
+                  value={minOrder}
+                  onChange={handleOrder}
+                  id="exampleFormControlInput1"
+                />
+              </div>
+              <p className="text-danger mt-2">{error}</p>
+
+              <button
+                disabled={
+                  minOrder < product.order || minOrder > product.quantity
+                }
+                className="btn  btn-primary w-100 text-white"
+              >
+                Order Now
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
