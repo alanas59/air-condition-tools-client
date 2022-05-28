1cc8cc6f74fc51a35349d16637454836b1e20d85
al-anas <alanas7171@gmail.com>
2022-05-28 15:22:19 +0600

    react query added

--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from "react";
+import {useQuery} from 'react-query'
 import Service from "./Service";
 
 const Services = () => {
-  const [services, setServices] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:5000/services")
-      .then((res) => res.json())
-      .then((data) => setServices(data));
-  }, []);
+  //const [services, setServices] = useState([]);
+
+  const {data:services} = useQuery('servicesData',()=> fetch('http://localhost:5000/services').then(res => res.json()));
+
+  // useEffect(() => {
+  //   fetch("")
+  //     .then((res) => res.json())
+  //     .then((data) => setServices(data));
+  // }, []);
+
   return (
     <div className="container my-5">
       <h2 className="text-center my-5">Our Services</h2>
       <div className="row">
          {
-              services.map(service => 
+              services?.map(service => 
                 <Service
                 service={service}
                 ></Service>)
